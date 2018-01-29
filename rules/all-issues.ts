cf068038d67ab8a544de977ce533d741fd29d693
Gustavo Barbosa <gustavocsb@gmail.com>
2018-01-29 18:43:43 -0500

    spread _wrap through ts files
    importing it isn't working quite well

--- rules/all-issues.ts
+++ rules/all-issues.ts
@@ -1,6 +1,19 @@
-import { message } from "danger"
-import { wrap } from "./_wrap"
+import { schedule, message } from "danger"
 
+// The inspiration for this is https://github.com/artsy/artsy-danger/blob/f019ee1a3abffabad65014afabe07cb9a12274e7/org/all-prs.ts
+const isJest = typeof jest !== "undefined"
+
+// Stores the parameter in a closure that can be invoked in tests.
+const _test = (reason: string, closure: () => void | Promise<any>) =>
+  // We return a closure here so that the (promise is resolved|closure is invoked)
+  // during test time and not when we call wrap().
+  () => (closure instanceof Promise ? closure : Promise.resolve(closure()))
+
+// Either schedules the promise for execution via Danger, or invokes closure.
+const _run = (reason: string, closure: () => void | Promise<any>) =>
+  closure instanceof Promise ? schedule(closure) : closure()
+
+export const wrap: any = isJest ? _test : _run
 export const hello = wrap("Hello from Peril", () => {
   message("Hello from Peril on your new issue.")
 })
--- rules/all-prs.ts
+++ rules/all-prs.ts
@@ -1,5 +1,19 @@
-import { danger, fail } from "danger"
-import { wrap } from "./_wrap"
+import { schedule, danger, fail } from "danger"
+
+// The inspiration for this is https://github.com/artsy/artsy-danger/blob/f019ee1a3abffabad65014afabe07cb9a12274e7/org/all-prs.ts
+const isJest = typeof jest !== "undefined"
+
+// Stores the parameter in a closure that can be invoked in tests.
+const _test = (reason: string, closure: () => void | Promise<any>) =>
+  // We return a closure here so that the (promise is resolved|closure is invoked)
+  // during test time and not when we call wrap().
+  () => (closure instanceof Promise ? closure : Promise.resolve(closure()))
+
+// Either schedules the promise for execution via Danger, or invokes closure.
+const _run = (reason: string, closure: () => void | Promise<any>) =>
+  closure instanceof Promise ? schedule(closure) : closure()
+
+export const wrap: any = isJest ? _test : _run
 
 export const needs_description = wrap("Every PR requires a description", () => {
   const pr = danger.github.pr
