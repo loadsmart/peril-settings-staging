b26000dc85aed453d79c1ef8ead791fca22cc260
Gustavo Barbosa <gustavocsb@gmail.com>
2018-04-12 19:16:42 -0400

    Update ios-prs.ts

--- rules/ios-prs.ts
+++ rules/ios-prs.ts
@@ -15,13 +15,13 @@ const _run = (reason: string, closure: () => void | Promise<any>) =>
 
 export const wrap: any = isJest ? _test : _run
 
-// export const viewFilesWereChanged = wrap("View-ish file changes require a screenshot ", () => {
-//   const extensions = [".xib", ".storyboard", "View.swift", "Button.swift"]
-//   const files = [...danger.git.modified_files, ...danger.git.created_files]
-//   const hasViewChanges = files.some(file => !!file.match(/\/views?\//i) || extensions.some(ext => file.endsWith(ext)))
-//   const prHasScreenshot = danger.github.pr.body.match(/https?:\/\/\S*\.(png|jpg|jpeg|gif){1}/)
+export const viewFilesWereChanged = wrap("View-ish file changes require a screenshot ", () => {
+  const extensions = [".xib", ".storyboard", "View.swift", "Button.swift"]
+  const files = [...danger.git.modified_files, ...danger.git.created_files]
+  const hasViewChanges = files.some(file => !!file.match(/\/views?\//i) || extensions.some(ext => file.endsWith(ext)))
+  const prHasScreenshot = danger.github.pr.body.match(/https?:\/\/\S*\.(png|jpg|jpeg|gif){1}/)
 
-//   if (hasViewChanges && !prHasScreenshot) {
-//     warn("View files were changed. Maybe you want to add a screenshot to your PR.")
-//   }
-// })
+  if (hasViewChanges && !prHasScreenshot) {
+    warn("View files were changed. Maybe you want to add a screenshot to your PR.")
+  }
+})
