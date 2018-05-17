73d633c766fbc08464f9ea8dcac8f16e455ecce9
Gustavo Barbosa <gustavocsb@gmail.com>
2018-05-16 23:27:11 -0400

    improve branch deletion rule

--- rules/delete-merged-pr-branch.ts
+++ rules/delete-merged-pr-branch.ts
@@ -18,17 +18,20 @@ export const wrap: any = isJest ? _test : _run
 export const deleteMergedPRBranch = wrap("Delete branch of a merged PR", async () => {
   const owner = danger.github.thisPR.owner
   const repo = danger.github.thisPR.repo
-  const ref = `heads/${danger.github.pr.head.ref}`
+  const branch = danger.github.pr.head.ref
 
-  console.info(`owner: ${owner}, repo: ${repo}, ref: ${ref}`)
+  if (!danger.github.pr.merged) {
+    console.info(`Branch ${branch} for PR #${danger.github.thisPR.number} is not merged yet.`)
+    return
+  }
 
   try {
-    console.info(`Deleting merged branch ${ref}`)
+    console.info(`Deleting merged branch ${branch}`)
     const api = danger.github.api
-    await api.gitdata.deleteReference({ owner, repo, ref: ref })
-    console.info(`Branch ${ref} deleted`)
+    await api.gitdata.deleteReference({ owner, repo, ref: `heads/${branch}` })
+    console.info(`Branch ${branch} deleted`)
   } catch (error) {
-    console.error(`Error deleting branch ${ref}`)
+    console.error(`Error deleting branch ${branch}`)
     console.error(error)
   }
 })
