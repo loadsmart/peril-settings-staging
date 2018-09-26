d1768c012642f4569f66b8ccbf8553fccd964633
Amim Knabben <amim@loadsmart.com>
2018-09-26 17:39:11 -0300

    Release and approve pR

---
+++ rules/release-prs.ts
@@ -0,0 +1,23 @@
+import { schedule, danger, message } from "danger"
+
+export const approveReleasePR = async () => {
+  const pr = danger.github.pr
+  if (pr.title.includes("[Release]")) {
+    const api = danger.github.api
+    const owner = pr.head.repo.owner.login
+    const repo = pr.head.repo.name
+    const number = pr.number
+    try {
+      api.pullRequests.createReview({
+        owner: owner,
+        repo: repo,
+        number: number,
+        event: "APPROVE",
+      })
+      await api.pullRequests.merge({ owner, repo, number, commit_title: "Merged by Peril" })
+    } catch (e) {
+      console.error("Error approving/merging release PR:")
+      console.error(e)
+    }
+  }
+}
