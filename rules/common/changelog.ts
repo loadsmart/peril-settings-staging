0dfc68da861849e821a07ed30567f5a637638264
Raphael Ribeiro <raphaelpr01@gmail.com>
2023-05-09 12:00:58 -0300

    ignore changelog check when project has semantic release

--- rules/common/changelog.ts
+++ rules/common/changelog.ts
@@ -4,14 +4,17 @@ import { danger, warn } from "danger"
 const changelog = async () => {
   const pr = danger.github.pr
   const changelogs_re = /(CHANGELOG.md|changelog.md|CHANGELOG.yml|changelogs\/.*)/
+  const semantic_release_re = /(\.releaserc\.(yaml|yml|json|js|cjs)|release\.config\.(js|cjs))$/
+
   const isOpen = pr.state === "open"
 
   const getContentParams = { path: "", owner: pr.head.user.login, repo: pr.head.repo.name }
   const rootContents: any = await danger.github.api.repos.getContents(getContentParams)
 
   const hasChangelog = rootContents.data.find((file: any) => changelogs_re.test(file.name))
+  const hasSemanticRelease = rootContents.data.find((file: any) => semantic_release_re.test(file.name))
 
-  if (isOpen && hasChangelog) {
+  if (isOpen && hasChangelog && !hasSemanticRelease) {
     const files = [...danger.git.modified_files, ...danger.git.created_files]
 
     const hasCodeChanges = files.find(file => !file.match(/(test|spec)/i))
